b8f48d947eac2eb1ea02a1d5c3c9426ba7d7c85b
michalmytych <michamytych@gmail.com>
2020-06-10 17:20:02 +0200

    updated app.py and portfolio route

--- app.py
+++ app.py
@@ -4,23 +4,6 @@ from config import *
 app = Flask(__name__)
 
 
-# Tmp placement for this data, will be pulled from db in the future:
-projects = [
-    {
-        'title': 'Tytuł projektu 1',
-        'content': 'opis projektu 1',
-        'img_url': 'static/tmp_img.jpg',
-        'date_posted': 'Miesiac 20, 2020'
-    },
-    {
-        'title': 'Tytuł projektu 1',
-        'content': 'opis projektu 2',
-        'img_url': 'static/tmp_img.jpg',
-        'date_posted': 'Miesiac 21, 2020'
-    }
-]
-
-
 @app.route("/")
 @app.route("/home")
 def home():
@@ -34,12 +17,19 @@ def blog():
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template('portfolio.html', title='My Portfolio', projects=projects)
+    return render_template(
+        'portfolio.html', 
+        title='My Portfolio', 
+        projects=projects)
 
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html', title='Contact')
+    return render_template(
+        'contact.html', 
+        title='Contact',
+        github_link=github_link,
+        mail=mail)
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 base_title = "Michał Mytych"
+mail = "m****************m"
+github_link = "https://github.com/michalmytych"
 
 # Tmp placement for this data, will be pulled from db in the future:
 projects = [
@@ -9,8 +11,14 @@ projects = [
         'date_posted': 'Miesiac 20, 2020'
     },
     {
-        'title': 'Tytuł projektu 1',
+        'title': 'Tytuł projektu 2',
         'content': 'opis projektu 2',
+        'img_url': 'static/tmp_img.jpg',
+        'date_posted': 'Miesiac 21, 2020'
+    },
+    {
+        'title': 'Tytuł projektu 3',
+        'content': 'opis projektu 3',
         'img_url': 'static/tmp_img.jpg',
         'date_posted': 'Miesiac 21, 2020'
     }
