8eafb5927531efbe55c1361f71fc9f888c923763
michalmytych <michamytych@gmail.com>
2020-06-11 20:25:26 +0200

    update admin

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from config import *
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app = Flask(__name__)
 app.config['FLASK_ADMIN_SWATCH'] = 'slate'
 
 admin = Admin(app, name='my_blog', template_mode='bootstrap3')
+admin.add_view(ModelView(Post, db.session))
 
 
 @app.route("/")
