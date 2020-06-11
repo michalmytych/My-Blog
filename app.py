61dbdf46c685b8103c2ff9099e248649f67723e6
michalmytych <michamytych@gmail.com>
2020-06-11 20:16:16 +0200

    create admin view

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, url_for
+from flask_admin import Admin
 from config import *
 
 app = Flask(__name__)
+
+app.config['FLASK_ADMIN_SWATCH'] = 'slate'
+
+admin = Admin(app, name='my_blog', template_mode='bootstrap3')
 
 
 @app.route("/")
--- config.py
+++ config.py
@@ -5,27 +5,6 @@ mail = "m****************m"
 github_link = "https://github.com/michalmytych"
 
 # Tmp placement for this data, will be pulled from db in the future:
-projects = [
-    {
-        'title': 'Tytuł projektu 1',
-        'content': 'opis projektu 1',
-        'img_url': 'static/tmp_img.jpg',
-        'date_posted': 'Miesiac 20, 2020'
-    },
-    {
-        'title': 'Tytuł projektu 2',
-        'content': 'opis projektu 2',
-        'img_url': 'static/tmp_img.jpg',
-        'date_posted': 'Miesiac 21, 2020'
-    },
-    {
-        'title': 'Tytuł projektu 3',
-        'content': 'opis projektu 3',
-        'img_url': 'static/tmp_img.jpg',
-        'date_posted': 'Miesiac 21, 2020'
-    }
-]
-
 
 posts = [
     {
