22c4f5db79e3c42f6aba5808d49c89ba5b5aeae7
Michal Mytych <michamytych@gmail.com>
2020-06-22 18:12:33 +0200

    fix import problem

--- app/__init__.py
+++ app/__init__.py
@@ -1,41 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
-from config import *
 import os
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Tmp placement for this data, will be pulled from db in the future:
-# This temporary model for development period
-
-base_title = "Michał Mytych"
-mail = "m****************m"
-github_link = "https://github.com/michalmytych"
-
-
-posts = [
-    {
-        'title': 'Tytuł postu 1',
-        'author': 'Michał Mytych',
-        'content': 'Tresc postu nr 1',
-        'img_url': 'static/tmp_img.jpg',
-        'date_posted': 'Miesiac 20, 2020'
-    },
-    {
-        'title': 'Tytuł postu 2',
-        'author': 'Michał Mytych',
-        'content': 'Tresc postu nr 2',
-        'img_url': 'static/tmp_img.jpg',
-        'date_posted': 'Miesiac 20, 2020'
-    },    {
-        'title': 'Tytuł postu 2',
-        'author': 'Michał Mytych',
-        'content': 'Tresc postu nr 2',
-        'img_url': 'static/tmp_img.jpg',
-        'date_posted': 'Miesiac 20, 2020'
-    }
-]
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
