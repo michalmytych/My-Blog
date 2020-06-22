01de9a423266b3f1b442231b0c52e967fcabfe01
Michal Mytych <michamytych@gmail.com>
2020-06-22 17:49:13 +0200

    move models to separate files

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from app import db
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(50), nullable=False)
+    content =  db.Column(db.String(500), unique = True, nullable=False)
+    img_url = db.Column(db.String(30), unique = True, nullable=True)
+    date_posted = db.Column(db.DateTime)
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    surname =  db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    message = db.Column(db.String(200), nullable=False)
+    date_send = db.Column(db.DateTime)
