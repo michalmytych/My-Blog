c6442c2c68879cb7a7a71fce0f7b2a7a4ae2933f
Michal Mytych <michamytych@gmail.com>
2020-06-22 21:18:02 +0200

    add admin views file

---
+++ app/admin_views.py
@@ -0,0 +1,11 @@
+from app import app
+
+
+@app.route("/admin/dashboard")
+def admin_dashboard():
+    return "here will be admin's dashboard"
+
+
+@app.route("/admin/posts")
+def admin_dashboard():
+    return "here will be admin's posts section"
