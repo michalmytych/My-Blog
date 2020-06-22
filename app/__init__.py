ed58622f3112af680da1662ef6743bef78b8267f
Michal Mytych <michamytych@gmail.com>
2020-06-22 17:49:57 +0200

    delete app.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from config import *
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['FLASK_ADMIN_SWATCH'] = 'slate'
+
+admin = Admin(app, name='my_blog', template_mode='bootstrap3')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '************'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mydatabase.db')
 db = SQLAlchemy(app)
+
 
 app.config.from_object(__name__)
 from app import views
