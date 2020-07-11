55f6e261c3fe80b0d7c6b9174a4132e13fd1e39f
michalmytych <michamytych@gmail.com>
2020-07-11 20:07:58 +0200

    add redirection to single post page

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ github_link = u"https://github.com/michalmytych"
 
 posts = [
     {
+        'id' : 0,
         'title': u'Tytul posta 1',
         'author': u'Michal Mytych',
         'content': u'Tresc posta nr 1',
@@ -14,6 +15,7 @@ posts = [
         'date_posted': u'Miesiac 20, 2020'
     },
     {
+        'id' : 1,
         #'title': ***just check if it works***,
         'author': u'Michal Mytych',
         'content': u'Tresc posta nr 2',
@@ -21,6 +23,7 @@ posts = [
         'date_posted': u'Miesiac 20, 2020'
     },    
     {
+        'id' : 2,
         'title': u'Tytuł posta 2',
         'author': u'Michal Mytych',
         'content': u'Tresc posta nr 2',
