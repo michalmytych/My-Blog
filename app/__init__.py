0e8edbb8b31839fb31e71ae6fa430255bfdea780
michalmytych <michamytych@gmail.com>
2020-07-15 15:32:56 +0200

    models update

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 import os
+from datetime import datetime
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -22,6 +23,20 @@ app.config['FLASK_ADMIN_SWATCH'] = 'slate'
 app.config.from_object(__name__)
 
 
-from app import views
-from app import admin_views
-from app import error_handlers
+# from app import views
+# from app import admin_views
+# from app import error_handlers
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(50), unique = True, nullable=False)
+    content =  db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(30), unique = True, nullable=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    thumbs_up = db.Column(db.Integer, default=0, nullable=True)
+    thumbs_down = db.Column(db.Integer, default=0, nullable=True)
+
+    def __repr__(self):
+        return f"Post(id:'{self.id}', title:'{self.title}')"
