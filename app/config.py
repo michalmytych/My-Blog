25d44231da681744ff2e9ae692966767c1400ab0
michalmytych <michamytych@gmail.com>
2020-07-19 15:01:34 +0200

    update app config

--- app/config.py
+++ app/config.py
@@ -1,4 +1,47 @@
 # -*- coding: utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+"""
+Command to change app configuration, for example:
+export APP_SETTINGS="config.DevelopmentConfig"
+
+export DATABASE_URL="postgresql://localhost/Posts"
+
+To violently kill app process running on port 5000:
+sudo kill -9 $(lsof -t -i:5000)
+"""
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    DATABASE_URL="postgresql://localhost/MyBlog"
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_ADMIN_SWATCH = 'slate'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
 
 base_title = u"Michal Mytych"
 mail = u"m****************m"
