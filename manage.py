47ee9ad761125abf16d70ae7f3c7b7eecbf1cefb
michalmytych <michamytych@gmail.com>
2020-07-19 15:07:48 +0200

    add manage file for database migrations

---
+++ manage.py
@@ -0,0 +1,19 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db
+
+"""
+To create database, being in My-Blog direction run commands:
+python manage.py db init
+python manage.py db migrate
+python manage.py db upgrade
+"""
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
