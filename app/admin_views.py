1d736524bb798b279960b99056ffba07d8fcd1fc
michalmytych <michamytych@gmail.com>
2020-08-24 13:58:10 +0200

    update admin/posts route

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, url_for
 from app import app
 
 
 @app.route("/admin")
 def admin():
     title = "Index of Admin"
-    return render_template("admin/index.html", title=title)
+    return render_template('admin.html',title=title)
 
 
 @app.route("/admin/dashboard")
 def admin_dashboard():
     title = "Dashboard"
-    return render_template("admin/index.html", title=title)
+    return render_template('admin.html',title=title)
 
 
 @app.route("/admin/posts")
 def admin_posts():
     title = "Posts"
-    return render_template("admin/index.html", title=title)
+    return render_template('posts.html',title=title)
 
 
 @app.route("/admin/posts/new")
 def admin_new_post():
     title = "New post"
-    return render_template("admin/index.html", title=title)
-
+    return render_template('newpost.html',title=title)
